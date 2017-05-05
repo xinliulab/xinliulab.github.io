31408a221f6817624f836b9cd7b95d5ffba4e2a6
LIUXIN <liuxin.bit@gmail.com>
2017-05-04 23:43:47 -0400

    Add JavaScript
    The target is to show all information by only one html page, that is
    the index.html.

--- ReadMe.md
+++ ReadMe.md
@@ -1,3 +1,3 @@
 Personal Website: https://swe.umbc.edu/~xinliu1/
 
-The browser is a parser rather than compiler. Because when it gets the code, it shows the corresponding content rather than make some deductive reasoning. 
+The browser is a parser rather than compiler. Because when it interprets the code, it shows the corresponding content rather than make some deductive reasoning. 
--- myScript.js
+++ myScript.js
@@ -1,3 +1,14 @@
-function myFunction() {
-   document.getElementById("demo").innerHTML = "Paragraph changed.";
+
+var strings_id_nav_tab = ["home", "publication", "project", "continue"];
+
+len = strings_id_nav_tab.length;
+
+for (var i = 0; i < len; i++) {
+	document.getElementById(strings_id_nav_tab[i]).addEventListener("click", display_nav_tab);
+
+};
+
+function display_nav_tab() {
+
+    document.getElementById(strings_id_nav_tab[0]).className = "active";
 }
