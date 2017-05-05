b1cd1fb937782d10264c469999eacc523eebe9f2
LIUXIN <liuxin.bit@gmail.com>
2017-05-05 14:25:02 -0400

    HTML+CSS+JS Version
    All of the content and effect can be displayed on only one html page.

--- myScript.js
+++ myScript.js
@@ -1,14 +1,43 @@
 
 var strings_id_nav_tab = ["home", "publication", "project", "continue"];
+var strings_id_content = ["content_home", "content_publication", "content_project"];
 
-len = strings_id_nav_tab.length;
+function display_home() {
+		document.getElementById(strings_id_nav_tab[0]).className = "active";  
+		document.getElementById(strings_id_nav_tab[1]).className = "state_idle";
+		document.getElementById(strings_id_nav_tab[2]).className = "state_idle";
+		document.getElementById(strings_id_nav_tab[3]).className = "state_idle";
+		document.getElementById(strings_id_content[0]).style.display = "initial";
+		document.getElementById(strings_id_content[1]).style.display = "none";
+		document.getElementById(strings_id_content[2]).style.display = "none";
+}
 
-for (var i = 0; i < len; i++) {
-	document.getElementById(strings_id_nav_tab[i]).addEventListener("click", display_nav_tab);
+function display_publication() {
+		document.getElementById(strings_id_nav_tab[0]).className = "state_idle";  
+		document.getElementById(strings_id_nav_tab[1]).className = "active"; 	
+		document.getElementById(strings_id_nav_tab[2]).className = "state_idle";
+		document.getElementById(strings_id_nav_tab[3]).className = "state_idle";
+		document.getElementById(strings_id_content[0]).style.display = "none";
+		document.getElementById(strings_id_content[1]).style.display = "initial";
+		document.getElementById(strings_id_content[2]).style.display = "none";
+}
 
-};
+function display_project() {
+		document.getElementById(strings_id_nav_tab[0]).className = "state_idle"; 
+		document.getElementById(strings_id_nav_tab[1]).className = "state_idle";	
+		document.getElementById(strings_id_nav_tab[2]).className = "active"; 
+		document.getElementById(strings_id_nav_tab[3]).className = "state_idle";
+		document.getElementById(strings_id_content[0]).style.display = "none";
+		document.getElementById(strings_id_content[1]).style.display = "none";
+		document.getElementById(strings_id_content[2]).style.display = "initial";
+}
 
-function display_nav_tab() {
-
-    document.getElementById(strings_id_nav_tab[0]).className = "active";
+function display_continue() {
+		document.getElementById(strings_id_nav_tab[0]).className = "state_idle";
+		document.getElementById(strings_id_nav_tab[1]).className = "state_idle"; 	
+		document.getElementById(strings_id_nav_tab[2]).className = "state_idle";
+		document.getElementById(strings_id_nav_tab[3]).className = "active"; 
+		document.getElementById(strings_id_content[0]).style.display = "none";
+		document.getElementById(strings_id_content[1]).style.display = "none";
+		document.getElementById(strings_id_content[2]).style.display = "none";
 }
